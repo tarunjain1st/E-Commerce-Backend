a2c7537ff3f25cfc00a88b0de30a135421853be7
Tarun Kumar Jain <tarunjain1st@gmail.com>
2026-01-24 17:09:48 +0530

    chore(user-management): enable service discovery with Eureka

---
+++ user-management/src/main/java/com/example/usermanagement/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.example.usermanagement.controllers;
+
+import com.example.usermanagement.dtos.UserDto;
+import com.example.usermanagement.models.User;
+import com.example.usermanagement.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public UserDto  getUser(@PathVariable Long id){
+        User user = userService.getUser(id);
+        return from(user);
+    }
+
+    private UserDto from(User user){
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+        return userDto;
+    }
+}
---
+++ user-management/src/main/java/com/example/usermanagement/services/UserService.java
@@ -0,0 +1,17 @@
+package com.example.usermanagement.services;
+
+import com.example.usermanagement.models.User;
+import com.example.usermanagement.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepo userRepo;
+
+    public User getUser(Long id){
+        User user = userRepo.findById(id).get();
+        return user;
+    }
+}
