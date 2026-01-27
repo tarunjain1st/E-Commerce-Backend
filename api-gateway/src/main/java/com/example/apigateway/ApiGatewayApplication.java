27180b0788b9c214935d743e8ba513b6ccfe4dd0
Tarun Kumar Jain <tarunjain1st@gmail.com>
2026-01-27 21:09:36 +0530

    fix(api-gateway): resolve routing issue

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,10 +1,7 @@
 package com.example.apigateway;
 
-import com.example.apigateway.security.JwtAuthenticationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
